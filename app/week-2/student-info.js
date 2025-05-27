e4ada7ad59e42983fdd392ee7fd531ba21696d71
MrKthulhu <Alshawimehdi@gmail.com>
2025-05-26 22:04:09 -0600

    Assignment 2 upload

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Mehdi Al-shawi</p>
+      <p>
+        <Link href="https://github.com/mrkthulhu/cprg306-assignments">
+          GitHub Repository
+        </Link>
+      </p>
+    </div>
+  );
+}
